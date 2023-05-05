c22b3ed4df8a5bf227b57773c0287af0ff06e9ff
OlegGolovko <oleg.golovko.job@gmail.com>
2023-05-05 11:47:13 +0200

    added event handlers for button and items

--- ishop2/ItemsComponent.js
+++ ishop2/ItemsComponent.js
@@ -1,37 +1,56 @@
 var ItemsComponent = React.createClass({
+  displayName: "ItemsComponent",
 
-    displayName: 'ItemsComponent',
+  propTypes: {
+    code: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string.isRequired,
+    price: React.PropTypes.number.isRequired,
+    qty: React.PropTypes.number.isRequired,
+    image: React.PropTypes.string.isRequired,
+    cbchangeBackground: React.PropTypes.func.isRequired,
+    cbdeleteItem: React.PropTypes.func.isRequired,
+    color: React.PropTypes.string.isRequired,
+  },
 
-    propTypes: {
-        code: React.PropTypes.number.isRequired,
-        name: React.PropTypes.string.isRequired,
-        price: React.PropTypes.number.isRequired,
-        qty: React.PropTypes.number.isRequired,
-        image: React.PropTypes.string.isRequired,
-        cbchangeBackground: React.PropTypes.func.isRequired,
-    },
+  changeBackground: function (eo) {
+    console.log("ItemsComponent - меня выбрали");
+    this.props.cbchangeBackground(this.props.code);
+  },
 
-    changeBackground: function (e) {
-        console.log("ItemsComponent - меня выбрали");
-        this.props.cbchangeBackground(this.props.code);
-    },
+  deleteItem: function (eo) {
+    console.log("ItemsComponent - меня удалили");
+    eo.stopPropagation();
+    this.props.cbdeleteItem(this.props.code);
+  },
 
-    render: function () {
-
-        return React.DOM.tr({
-            className: 'TabTr',
+  render: function () {
+    return React.DOM.tr(
+      {
+        style: { backgroundColor: this.props.color },
+        className: "TabTr",
+        id: this.props.code,
+        onClick: this.changeBackground,
+      },
+      React.DOM.td({ className: "TabTd" }, this.props.name),
+      React.DOM.td({ className: "TabTd" }, this.props.price),
+      React.DOM.td({ className: "TabTd" }, this.props.qty),
+      React.DOM.td(
+        { className: "TabTd" },
+        React.createElement("img", { src: this.props.image })
+      ),
+      React.DOM.td(
+        { className: "TabTd" },
+        React.DOM.div(
+          null,
+          React.DOM.input({
+            type: "button",
+            className: "btn-delete",
+            value: "Delete",
             id: this.props.code,
-            onClick: this.changeBackground,
-        },
-            React.DOM.td({ className: 'TabTd' }, this.props.name),
-            React.DOM.td({ className: 'TabTd' }, this.props.price),
-            React.DOM.td({ className: 'TabTd' }, this.props.qty),
-            React.DOM.td({ className: 'TabTd' },
-                React.createElement('img', { src: this.props.image })
-            ),
-            React.DOM.td({ className: 'TabTd' },
-                React.DOM.input({ type: 'button', className: 'btn-delete', value: "Delete", }),
-            ),
+            onClick: this.deleteItem,
+          })
         )
-    },
+      )
+    );
+  },
 });
--- ishop2/ShopComponent.js
+++ ishop2/ShopComponent.js
@@ -1,52 +1,87 @@
 var ShopComponent = React.createClass({
+  displayName: "ShopComponent",
 
-    displayName: 'ShopComponent',
+  propTypes: {
+    column1: React.PropTypes.string.isRequired,
+    column2: React.PropTypes.string.isRequired,
+    column3: React.PropTypes.string.isRequired,
+    column4: React.PropTypes.string.isRequired,
+    column5: React.PropTypes.string.isRequired,
+    bgcolor: React.PropTypes.string.isRequired,
+    items: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        image: React.PropTypes.string.isRequired,
+        qty: React.PropTypes.number.isRequired,
+      })
+    ),
+  },
 
-    propTypes: {
-        column1: React.PropTypes.string.isRequired,
-        column2: React.PropTypes.string.isRequired,
-        column3: React.PropTypes.string.isRequired,
-        column4: React.PropTypes.string.isRequired,
-        column5: React.PropTypes.string.isRequired,
-        items: React.PropTypes.arrayOf(
-            React.PropTypes.shape({
-                code: React.PropTypes.number.isRequired,
-                name: React.PropTypes.string.isRequired,
-                price: React.PropTypes.number.isRequired,
-                image: React.PropTypes.string.isRequired,
-                qty: React.PropTypes.number.isRequired,
-            })
-        )
-    },
+  getInitialState: function () {
+    return {
+      selectedItemCode: null,
+      selectedDeleteItemCode: null,
+    };
+  },
 
-    changeBackground: function (a) {
-        console.log("ShopComponent - меня выбрали " + "№ " + a);
-    },
+  changeBackground: function (cd) {
+    console.log("ShopComponent - меня выбрали " + "№ " + cd);
+    this.setState({ selectedItemCode: cd });
+    console.log(this.state.selectedItemCode);
+  },
 
-    render: function () {
+  deleteItem: function (cd) {
+    console.log("ShopComponent - меня удалили " + "№ " + cd);
+    this.setState({ selectedDeleteItemCode: cd });
+    console.log(this.state.selectedDeleteItemCode);
+  },
 
-        var itemsCode = this.props.items.map(v =>
-            React.createElement(ItemsComponent, {
-                key: v.code,
-                name: v.name,
-                price: v.price,
-                qty: v.qty,
-                image: v.image,
-                cbchangeBackground: this.changeBackground
-            })
-        );
+  render: function () {
+    if (this.state.selectedItemCode != this.props.code) {
+      var itemsCode = this.props.items.map((v) =>
+        React.createElement(ItemsComponent, {
+          key: v.code,
+          code: v.code,
+          name: v.name,
+          price: v.price,
+          qty: v.qty,
+          image: v.image,
+          cbchangeBackground: this.changeBackground,
+          cbdeleteItem: this.deleteItem,
+          color: this.props.bgcolor,
+        })
+      );
+    } else {
+      itemsCode = this.props.items.map((v) =>
+        React.createElement(ItemsComponent, {
+          key: v.code,
+          code: v.code,
+          name: v.name,
+          price: v.price,
+          qty: v.qty,
+          image: v.image,
+          cbchangeBackground: this.changeBackground,
+          cbdeleteItem: this.deleteItem,
+        })
+      );
+    }
 
-        return React.DOM.table({ className: 'ShopComponentTable' },
-            React.DOM.tbody({ className: 'TBody' },
-                React.DOM.tr({ className: 'TabTr' },
-                    React.DOM.th({ className: 'TabTh' }, this.props.column1),
-                    React.DOM.th({ className: 'TabTh' }, this.props.column2),
-                    React.DOM.th({ className: 'TabTh' }, this.props.column3),
-                    React.DOM.th({ className: 'TabTh' }, this.props.column4),
-                    React.DOM.th({ className: 'TabTh' }, this.props.column5),
-                ),
-                itemsCode,
-            ),
-        );
-    },
+    return React.DOM.table(
+      { className: "ShopComponentTable" },
+      React.DOM.tbody(
+        { className: "TBody" },
+        React.DOM.tr(
+          { className: "TabTr" },
+          React.DOM.th({ className: "TabTh" }, this.props.column1),
+          React.DOM.th({ className: "TabTh" }, this.props.column2),
+          React.DOM.th({ className: "TabTh" }, this.props.column3),
+          React.DOM.th({ className: "TabTh" }, this.props.column4),
+          React.DOM.th({ className: "TabTh" }, this.props.column5)
+        ),
+        itemsCode
+      )
+    );
+  },
 });
