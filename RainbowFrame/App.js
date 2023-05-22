cf7f1660ec46c48b439c383a5c52e9766b7304f0
OlegGolovko <oleg.golovko.job@gmail.com>
2023-05-23 00:27:45 +0200

    start RainbowFrame

---
+++ RainbowFrame/App.js
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import RainbowFrame from "./components/RainbowFrame.js";
+
+let colorArr = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "#00BFFF",
+  "blue",
+  "purple",
+];
+
+ReactDOM.render(
+  <RainbowFrame colors={colorArr}>Hello!</RainbowFrame>,
+  document.getElementById("container")
+);
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,28 @@
+﻿import React from "react";
+import PropTypes, { element } from "prop-types";
+import DOM from "react-dom-factories";
+
+import "./RainbowFrame.css";
+
+class RainbowFrame extends React.Component {
+  static propTypes = {
+    colors: PropTypes.array,
+  };
+
+  state = {
+    color: " ",
+  };
+
+  render() {
+    const borderStyle = {
+      borderColor: this.state.color,
+      borderWidth: 10,
+      borderStyle: "solid",
+      textAlign: "center",
+    };
+
+    return <div style={borderStyle}>{this.props.children}</div>;
+  }
+}
+
+export default RainbowFrame;
