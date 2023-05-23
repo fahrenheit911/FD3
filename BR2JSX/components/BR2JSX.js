6d994d1c7f24aee644fe473742e95321ec5da468
OlegGolovko <oleg.golovko.job@gmail.com>
2023-05-23 23:49:01 +0200

    created a component to push the tags to the string

---
+++ BR2JSX/components/BR2JSX.js
@@ -0,0 +1,26 @@
+﻿import React, { Fragment } from "react";
+import PropTypes, { element } from "prop-types";
+import DOM from "react-dom-factories";
+
+import "./BR2JSX.css";
+
+class BR2JSX extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+
+  render() {
+    const re = /<br *\/?>/g;
+    const textLine = this.props.text.split(re);
+    const textArr = [];
+
+    for (let i = 0; i < textLine.length; i++) {
+      if (i) textArr.push(<br />);
+      textArr.push(textLine[i]);
+    }
+
+    return <div>{textArr}</div>;
+  }
+}
+
+export default BR2JSX;
